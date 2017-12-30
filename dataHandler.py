6c689ec66635c4d740130131fe6847610d489391
Andrea Galloni <andreagalloni92@gmail.com>
2017-12-30 05:47:56 +0100

    Created historicalDataQueue and dataHandler

---
+++ dataHandler.py
@@ -0,0 +1,93 @@
+from dateutil import parser
+from datetime import timedelta
+
+import numpy as np
+import warnings
+import pickle
+
+
+class DataHandler:
+
+    def __init__(self, path_builder,):
+        self.pb = path_builder
+        self.rtd = self.load_rtd()
+        self.translator = self.load_translator()
+
+    def load_rtd(self):
+        print('>[load_relevant_towers(path)]\nLOADING RELEVANT TOWERS\n---')
+        with open(self.pb.get_relevant_towers_path(), 'r') as tower_f:
+            return {l.strip().split(';')[0]:
+                        ';'.join(l.strip().split(';')[-2:])
+                        for l in tower_f.readlines()}
+
+    def load_translator(self):
+        with open(self.pb.get_translator_path(), 'rb') as fobj:
+            return pickle.load(fobj)
+
+    def load_past_data(self, date_str, n_days):
+        files_to_load = []
+        my_date = parse(date_str)
+        my_delta = timedelta(days=1)
+        for i in range(n_days):
+            my_date = my_date - my_delta
+            files_to_load.append(
+                self.pb.build_past_data_file_name(my_date.strftime('%Y%m%d')))
+
+        past_data = []
+        for filename in files_to_load:
+            try:
+                with open(self.pb.build_historical_path(filename), 'rb')\
+                            as my_file:
+                    past_data.append(pickle.load(my_file))
+            except FileNotFoundError:
+                print("File not found:" + self.pb.
+                      build_historical_path(filename))
+
+        print("Historical data loaded!")
+        return self.create_data_structure(past_data, n_days)
+
+    def create_data_structure(self, past_data_list, n_days):
+        print('>[create_data_structure(past_data_list, number_of_slices,' +
+              ' number_of_days)]\nCREATING DATA STRUCTURE\n---')
+        tower_past_data_structs = {}
+        for tower_id in set(self.rtd.values()):
+            matrix = np.zeros(shape=(1440, n_days + 3),
+                              dtype=np.float)
+
+            for idx, past_data in enumerate(past_data_list):
+                my_list = np.full((1440), np.nan)
+
+                if tower_id in past_data:
+                    my_list = np.asarray(past_data[tower_id], np.float)
+
+                matrix[:, idx] = my_list
+
+            # eventually parallelize for loop
+            # I expect to see RuntimeWarnings in this block
+            with warnings.catch_warnings():
+                warnings.simplefilter("ignore", category=RuntimeWarning)
+                for idx, val in enumerate(matrix):
+                    matrix[idx, -3] = np.nanmean(val[:-3])
+                    matrix[idx, -2] = np.nanstd(val[:-3])
+
+            tower_past_data_structs[tower_id] = matrix
+
+        print('>[create_data_structure(past_data_list, number_of_slices,' +
+              ' number_of_days)]\nDATA STRUCTURE CREATED\n---')
+        return tower_past_data_structs
+
+    def persist(self, log_date=None):
+
+        file_name = self.pb.build_past_data_file_name(
+                                        self.last_date_seen.replace('-', ''))
+        with open(self.pb.build_historical_path(file_name), 'wb') as fd:
+            pickle.dump(dict(self.KP.get_raw_snapshot()), fd)
+
+        print(dict(self.KP.get_raw_snapshot()))
+        print("Stored: {}".format(file_name))
+
+        if log_date is not None:
+            self.last_date_seen = log_date
+
+        # TODO move it back to LogProcessor
+        self.KP.reset()
---
+++ historicalDataQueue.py
@@ -0,0 +1,83 @@
+from collections import deque
+
+import numpy as np
+
+class HistoricalDataQueue(deque):
+
+    def __init__(self, iterable=(), maxlen=None):
+        super().__init__(iterable, maxlen)
+        self.mean = np.nanmean(self)
+        self.std = np.nanstd(self)
+        self.sum = np.nansum(self)
+
+    def __update(self):
+        print ("Update")
+        self.mean = np.nanmean(self)
+        self.std = np.nanstd(self)
+
+    def __short_update(self, new=np.nan, old=np.nan):
+        self.sum = np.nansum((self.sum, - old, new))
+        self.mean = self.sum / len(self)
+        self.std = np.nanstd(self)
+
+    def __repr__(self):
+        return "HistoricalData(" + super().__repr__() + ", mean=" + \
+                                str(self.mean) + ", std=" + str(self.std) + ")"
+
+    def __str__(self):
+        return self.__repr__()
+
+    def append(self,x):
+        old = np.nan
+        if len(self) == self.maxlen:
+            old = self[0]
+
+        super().append(x)
+        self.__short_update(x, old)
+
+    def appendleft(self, x):
+        old = np.nan
+        if len(self) == self.maxlen:
+            old = self[-1]
+
+        super().appendleft(x)
+        self.__short_update(x, old)
+
+    def pop(self):
+        res = super().pop()
+        self.__short_update(old=res)
+        return res
+
+    def popleft(self):
+        res = super().popleft()
+        self.__short_update(old=res)
+        return res
+
+    def clear(self):
+        res = super().clear()
+        self.mean, self.std, self.sum = 0,0,0
+
+    def extend(self, iterable):
+        res = super().extend(iterable)
+        self.__update()
+        return res
+
+    def extendleft(self, iterable):
+        res = super().extendleft(iterable)
+        self.__update()
+        return res
+
+    def index(self, x, start=None, stop=None):
+        res = super().index(x, start, stop)
+        self.__update()
+        return res
+
+    def insert(self, i, x):
+        res = super().insert(i, x)
+        self.__update()
+        return res
+
+    def remove(self, value):
+        res = super().remove(value)
+        self.__update()
+        return res
