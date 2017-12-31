e4e9db367d7bba852b1ab0af7684dd24725c85e8
Andrea Galloni <andreagalloni92@gmail.com>
2017-12-31 05:58:48 +0100

    fixed import date parser bug, reimplemented create_data_structure making use of HistoricalDataQueue ds

--- dataHandler.py
+++ dataHandler.py
@@ -1,4 +1,5 @@
-from dateutil import parser
+from historicalDataQueue import HistoricalDataQueue as HDQ
+from dateutil.parser import parse
 from datetime import timedelta
 
 import numpy as np
@@ -49,28 +50,19 @@ class DataHandler:
     def create_data_structure(self, past_data_list, n_days):
         print('>[create_data_structure(past_data_list, number_of_slices,' +
               ' number_of_days)]\nCREATING DATA STRUCTURE\n---')
+
         tower_past_data_structs = {}
         for tower_id in set(self.rtd.values()):
-            matrix = np.zeros(shape=(1440, n_days + 3),
-                              dtype=np.float)
+            tower_queues_list = [None] * 1440
+            for ith_slot in range(1440):
+                tower_historical = [np.nan] * n_days
+                for idx, daily_data in enumerate(past_data_list):
+                    if tower_id in daily_data:
+                        tower_historical[idx] = daily_data[tower_id][ith_slot]
+                tower_queues_list[ith_slot] = HDQ(tower_historical)
+                tower_queues_list[ith_slot].reverse()
+            tower_past_data_structs[tower_id] = tower_queues_list
 
-            for idx, past_data in enumerate(past_data_list):
-                my_list = np.full((1440), np.nan)
-
-                if tower_id in past_data:
-                    my_list = np.asarray(past_data[tower_id], np.float)
-
-                matrix[:, idx] = my_list
-
-            # eventually parallelize for loop
-            # I expect to see RuntimeWarnings in this block
-            with warnings.catch_warnings():
-                warnings.simplefilter("ignore", category=RuntimeWarning)
-                for idx, val in enumerate(matrix):
-                    matrix[idx, -3] = np.nanmean(val[:-3])
-                    matrix[idx, -2] = np.nanstd(val[:-3])
-
-            tower_past_data_structs[tower_id] = matrix
 
         print('>[create_data_structure(past_data_list, number_of_slices,' +
               ' number_of_days)]\nDATA STRUCTURE CREATED\n---')
